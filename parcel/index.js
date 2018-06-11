ffb2c71d555827d5261f5221c90db8bcc02132b5
Mattemyo <matias97marti@gmail.com>
2018-06-11 14:14:14 +0200

    parcel tutorial

---
+++ parcel/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+const Hello = () => <div>What's up?</div>;
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
